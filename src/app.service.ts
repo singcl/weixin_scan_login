ea0c423fce2f744093a56ed54fa11b097fb0cedf
singcl <1276837689@qq.com>
2022-12-16 20:34:57 +0800

    chore(utils): utils

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType /* ConfigService */ } from '@nestjs/config';
 import { UtilsService } from './utils/services/utils.service';
-import * as crypto from 'crypto';
-// 不能写成这样：
-// import crypto from 'crypto';
 import { WxCheckSignatureDto } from './dto/wx-check-signature.dto';
 
 import { config } from './config';
@@ -15,17 +12,12 @@ export class AppService {
     private readonly utilsService: UtilsService,
   ) {}
   getHello(): string {
-    console.log('-----utils:', this.utilsService.myCustomUtil());
     return 'Hello World!';
   }
 
   // 验证消息的确来自微信服务器
   wxCheckSignature(query: WxCheckSignatureDto): string {
     const TOKEN = this.appConfig.params.weixinMpCheckToken;
-    const { signature, timestamp, nonce, echostr } = query;
-    const str = [TOKEN, timestamp, nonce].sort().join('');
-    const signatureCurr = crypto.createHash('sha1').update(str).digest('hex');
-    if (signatureCurr === signature) return echostr;
-    return null;
+    return this.utilsService.checkWxSha1Equal(query, TOKEN);
   }
 }
--- src/utils/services/utils.service.ts
+++ src/utils/services/utils.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
-
+import * as crypto from 'crypto';
+// 不能写成这样：
+// import crypto from 'crypto';
+import { WxCheckSignatureDto } from '../../dto/wx-check-signature.dto';
 @Injectable()
 export class UtilsService {
-  myCustomUtil() {
-    return 'this is an util';
+  // sha1加密
+  getSha1(str: string) {
+    return crypto.createHash('sha1').update(str).digest('hex');
+  }
+  checkWxSha1Equal(query: WxCheckSignatureDto, token: string) {
+    const { signature, timestamp, nonce, echostr } = query;
+    const str = [token, timestamp, nonce].sort().join('');
+    const signatureCurr = this.getSha1(str);
+    if (signatureCurr === signature) return echostr;
+    return null;
   }
 }
