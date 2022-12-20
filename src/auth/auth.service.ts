b58f8ace4a499e7e21c6ff313f9d24ae4476d5de
singcl <1276837689@qq.com>
2022-12-20 22:13:55 +0800

    feat(core): 同一个账号能同时登录3台设备方案。

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -44,29 +44,56 @@ export class AuthService {
       };
     }
 
-    const salt = this.appConfig.params.weixinLoginSalt;
-    const token = this.utilsService.genRandomToken(openid, salt);
-    const ttl = 30 * 60 * 1000;
+    //
     // 设置两个缓存，同时记录同一个账号的登录数量
     // @see https://zhuanlan.zhihu.com/p/508554307
-    // 一个账号能同时登录3台设备终极方案。
+    // 同一个账号能同时登录3台设备方案。
     // 1、同样的为一个账号登录的时候，都给其分配一个 token .
     // token在 redis中的对应关系还是【 key: token , value: userId 】，
     // 然后 userId 在 redis中的对应关系也还是【 key: userId , value: userInfo 】。
     // 但是 userInfo 里面不再是用户的基本信息了，这里面需要增加一个属性 loginTokenList ,用来保存此账号已经登录的 token 集合。
     // 2、当账号登录的时候，根据 userId 取出 userInfo ,然后拿到loginTokenList ，逐一判断 token 是否失效，
-    // 然后简单判断一下过滤后的loginTokenListsize 即可。注意：这里更新 userInfo 信息时候同样是需要原子操作的（ lua 脚本），
+    // 然后简单判断一下过滤后的loginTokenListSize 即可。注意：这里更新 userInfo 信息时候同样是需要原子操作的（ lua 脚本），
     // 因为是登录，所以明显这里的并发要小很多，所以这里原子性的更新对 redis 系统几乎没有影响。
     // 3、当用户访问页面的时候，我们还是同样的根据 token 获取 userId ,
     //  再根据 userId 获取 userInfo 。这里同上，用户访问页面的时候尽量不要去检查和更新 userInfo 里的loginTokenList。
-    const tokenKey = `wechat:login_user:${token}`;
-    const openidKey = `wechat:login_openid:${openid}`;
     //
-    await this.cacheManager.set(tokenKey, openidKey, ttl);
-    const user: Record<string, any> | null = await this.cacheManager.get(
+    const openidKey = `wechat:login_openid:${openid}`;
+
+    const salt = this.appConfig.params.weixinLoginSalt;
+    const token = this.utilsService.genRandomToken(openid, salt);
+    const ttl = 30 * 60 * 1000;
+    const tokenKey = `wechat:login_user:${token}`;
+    const userInfo: Record<string, any> | null = await this.cacheManager.get(
       openidKey,
     );
-    if (!user) {
+    if (userInfo) {
+      const loginTokenList: string[] = userInfo.loginTokenList || [];
+      const loginTokenList2: string[] = [];
+      for (let i = 0; i < loginTokenList.length; i++) {
+        const tkKey = loginTokenList[i];
+        const oId = await this.cacheManager.get(tkKey);
+        if (oId) {
+          loginTokenList2.push(tkKey);
+        }
+      }
+      if (loginTokenList2.length >= 3) {
+        return {
+          success: false,
+          token: null,
+          message: '同一个账号最多在3台设备上登录',
+        };
+      }
+      userInfo.loginTokenList = loginTokenList2;
+      await this.cacheManager.set(
+        openidKey,
+        {
+          ...userInfo,
+          loginTokenList: [...loginTokenList2, tokenKey],
+        },
+        ttl,
+      );
+    } else {
       await this.cacheManager.set(
         openidKey,
         {
@@ -75,13 +102,9 @@ export class AuthService {
         },
         ttl,
       );
-    } else {
-      const res2 = {
-        ...user,
-        loginTokenList: [...user.loginTokenList, tokenKey],
-      };
-      await this.cacheManager.set(openidKey, res2, ttl);
     }
+    //
+    await this.cacheManager.set(tokenKey, openidKey, ttl);
     await this.cacheManager.del(sessionKey);
     return {
       success: true,
