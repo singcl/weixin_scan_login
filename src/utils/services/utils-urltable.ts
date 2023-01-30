ba073fb4c56ea8266cdc1a754930893f94ac170f
singcl <1276837689@qq.com>
2023-01-30 19:15:14 +0800

    feat(utils): utils url table mapping.

---
+++ src/utils/services/utils-urltable.ts
@@ -0,0 +1,142 @@
+// 在ts中导入export = x 的模块定义
+import trimStart = require('lodash/trimStart');
+import trim = require('lodash/trim');
+
+type Section = {
+  leaf: boolean;
+  mapping: Record<string, Section>;
+};
+
+// type Table = {
+//   size: number;
+//   root: Section;
+// };
+
+type Result = { success: boolean; msg?: string; data?: string[] };
+
+export class UtilsUrltable {
+  empty = '';
+  fuzzy = '*';
+  omitted = '**';
+  delimiter = '/';
+  methodView = 'VIEW';
+
+  //
+  size = 0;
+  root = {
+    leaf: false,
+    mapping: {},
+  };
+
+  // parse and validate pattern
+  parse(p: string): Result {
+    const format = '[get, post, put, patch, delete, view]/{a-Z}+/{*}+/{**}';
+    const pattern = trimStart(trim(p), this.delimiter);
+    if (pattern === '') {
+      return {
+        success: false,
+        msg: 'pattern illegal, should in format of ' + format,
+      };
+    }
+    let paths = pattern.split(this.delimiter);
+    if (paths.length < 2) {
+      return {
+        success: false,
+        msg: 'pattern illegal, should in format of ' + format,
+      };
+    }
+    paths = paths.map((m) => trim(m));
+    // likes get/ get/* get/**
+    if (
+      paths.length === 2 &&
+      (paths[1] == this.empty ||
+        paths[1] == this.fuzzy ||
+        paths[1] == this.omitted)
+    ) {
+      return {
+        success: false,
+        msg: 'illegal wildcard',
+      };
+    }
+    paths[0] = paths[0].toUpperCase();
+    switch (paths[0]) {
+      case 'GET':
+      case 'POST':
+      case 'PUT':
+      case 'PATCH':
+      case 'DELETE':
+      case this.methodView:
+        break;
+      default:
+        return {
+          success: false,
+          msg: `only supports [GET POST PUT PATCH DELETE ${this.methodView}]`,
+        };
+    }
+    for (let k = 1; k < paths.length; k++) {
+      if (paths[k] == this.empty && k + 1 != paths.length) {
+        return {
+          success: false,
+          msg: `pattern contains illegal empty path`,
+        };
+      }
+
+      if (paths[k] == this.omitted && k + 1 != paths.length) {
+        return {
+          success: false,
+          msg: `pattern contains illegal omitted path`,
+        };
+      }
+    }
+    return {
+      success: true,
+      data: paths,
+    };
+  }
+
+  //
+  newSection(): Section {
+    return {
+      leaf: false,
+      mapping: {},
+    };
+  }
+
+  //
+  append(pattern: string): Result {
+    const parseResult = this.parse(pattern);
+    if (!parseResult.success) {
+      return parseResult;
+    }
+    const paths = parseResult.data;
+    let insert = false;
+    let root = this.root;
+    for (let i = 0; i < paths.length; i++) {
+      const path = paths[i];
+      if (
+        (path == this.fuzzy && root.mapping[this.omitted] != null) ||
+        (path == this.omitted && root.mapping[this.fuzzy] != null) ||
+        (path != this.omitted && root.mapping[this.omitted] != null)
+      ) {
+        return {
+          success: false,
+          msg: `conflict at ${paths.slice(0, i).join(this.delimiter)}`,
+        };
+      }
+      let next = root.mapping[path];
+      if (next == null) {
+        next = this.newSection();
+        root.mapping[path] = next;
+        insert = true;
+      }
+      root = next;
+    }
+    if (insert) {
+      this.size += 1;
+    }
+    root.leaf = true;
+    return {
+      success: true,
+    };
+  }
+}
--- src/utils/utils.module.ts
+++ src/utils/utils.module.ts
@@ -1,10 +1,19 @@
 import { Module, Global } from '@nestjs/common';
 
+import { UtilsUrltable } from './services/utils-urltable';
 import { UtilsService } from './services/utils.service';
 
 @Global()
 @Module({
-  providers: [UtilsService],
-  exports: [UtilsService],
+  providers: [
+    UtilsService,
+    {
+      provide: 'URL_TABLE',
+      useFactory: () => {
+        return new UtilsUrltable();
+      },
+    },
+  ],
+  exports: [UtilsService, 'URL_TABLE'],
 })
 export class UtilsModule {}
